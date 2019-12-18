2ae44b399a6eeffe884da3a21baa8297125180bd
Tom Jans <tjans@student.codam.nl>
2019-12-18 18:52:41 +0100

    feat: flag parser now parses length
    Only h and hh are supported for now
    Issue #5

---
+++ inc/printf_bonus.h
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   printf_bonus.h                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: tjans <tjans@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/11/26 11:23:53 by tjans         #+#    #+#                 */
+/*   Updated: 2019/12/18 18:42:31 by tjans         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTF_BONUS_H
+# define PRINTF_BONUS_H
+# include <stdarg.h>
+
+typedef enum	e_sizemod
+{
+	DEFAULT,
+	SHORT,
+	CHAR
+}				t_sizemod;
+
+typedef struct	s_flags
+{
+	char		pad_neg_f_width;
+	char		pad_zeroes;
+	char		min_field_width;
+	char		precision;
+	char		print_null_term;
+
+	t_sizemod	sizemod;
+
+	int			min_field_width_n;
+	int			precision_n;
+}				t_flags;
+
+typedef struct	s_ctable
+{
+	char	c_arg;
+	char	*(*conv)(va_list args, t_flags *flags);
+}				t_ctable;
+
+extern t_ctable	g_ctable[];
+t_ctable		*find_conv(char c);
+
+char			*ltoa_base_unsigned(unsigned long n, int base);
+char			*ltoa_signed(long n);
+
+t_flags			*parse_flags(const char **fmt, va_list args);
+int				print_str(const char *fmt, va_list args);
+int				ft_printf(const char *fmt, ...);
+
+#endif
---
+++ src/parse_flag_bonus.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   parse_flag.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: tjans <tjans@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/11/26 11:29:16 by tjans         #+#    #+#                 */
+/*   Updated: 2019/12/18 18:51:17 by tjans         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "utils.h"
+#include "printf_bonus.h"
+
+static int	get_precision(const char **fmt, va_list args, t_flags *flags)
+{
+	int	precision;
+
+	precision = 0;
+	if (**fmt == '*')
+	{
+		precision = va_arg(args, int);
+		(*fmt)++;
+		if (precision < 0)
+		{
+			precision *= -1;
+			if (!flags->precision)
+				flags->pad_neg_f_width = 1;
+			else
+				flags->precision = 0;
+		}
+		return (precision);
+	}
+	while (ft_isdigit(**fmt))
+	{
+		precision *= 10;
+		precision += **fmt - '0';
+		(*fmt)++;
+	}
+	return (precision);
+}
+
+static void	set_flags(t_flags *flags, const char **fmt)
+{
+	while (**fmt == '-' || **fmt == '0')
+	{
+		if (**fmt == '-')
+		{
+			flags->pad_neg_f_width = 1;
+			(*fmt)++;
+		}
+		if (**fmt == '0')
+		{
+			flags->pad_zeroes = 1;
+			(*fmt)++;
+		}
+	}
+}
+
+static void	set_modififers(t_flags *flags, const char **fmt)
+{
+	if (**fmt == 'h' && (**fmt) + 1 == 'h')
+		flags->sizemod = CHAR;
+	else if (**fmt == 'h')
+		flags->sizemod = SHORT;
+}
+
+static void	set_arg(t_flags *flags, const char **fmt, va_list args)
+{
+	set_flags(flags, fmt);
+	if (ft_isdigit(**fmt) || **fmt == '*')
+	{
+		flags->min_field_width = 1;
+		flags->min_field_width_n = get_precision(fmt, args, flags);
+	}
+	if (**fmt == '.')
+	{
+		(*fmt)++;
+		flags->precision = 1;
+		flags->precision_n = get_precision(fmt, args, flags);
+	}
+	set_modififers(flags, fmt);
+}
+
+t_flags		*parse_flags(const char **fmt, va_list args)
+{
+	t_flags	*flags;
+
+	(*fmt)++;
+	flags = ft_calloc(1, sizeof(t_flags));
+	if (!flags)
+		return (NULL);
+	set_arg(flags, fmt, args);
+	return (flags);
+}
